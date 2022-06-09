5ceadb56d23bc7acfc761af03a46f0e3fe50684c
ryanallenk <ryanallenk@gmail.com>
2022-06-09 13:32:04 -0400

    add global styles

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,5 +1,20 @@
 import PropTypes from 'prop-types';
+import { createGlobalStyle } from 'styled-components';
 import Header from './Header';
+
+const GlobalStyles = createGlobalStyle`
+  html {
+    --red: #ff0000;
+    --black: #393939;
+    --grey: #3a3a3a;
+    --gray: var(--grey);
+    --lightgrey: #e1e1e1;
+    --lighgray: var(--lighgrey);
+    --offwhite: #ededed;
+    --maxWidth: 1000px;
+    --bs: 0 12px 24px 0 rgba(0,0,0,0.99);
+  }
+`;
 
 export default function Page({ children, cool }) {
   return (
