b4201003622c5b0962a389e4fe338adba7106179
ryanallenk <ryanallenk@gmail.com>
2022-06-09 17:08:12 -0400

    add visualizing route changes on Header

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -3,6 +3,21 @@ import styled from 'styled-components';
 import Link from 'next/link';
 import Nav from './Nav';
 
+const HeaderStyles = styled.header`
+  .bar {
+    border-bottom: 10px solid var(--black, black);
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .sub-bar {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    border-bottom: 1px solid var(--black, black);
+  }
+`;
+
 const LogoStyles = styled.h1`
   font-size: 4rem;
   margin-left: 2rem;
@@ -18,20 +33,6 @@ const LogoStyles = styled.h1`
   }
 `;
 
-const HeaderStyles = styled.header`
-  .bar {
-    border-bottom: 10px solid var(--black, black);
-    display: grid;
-    grid-template-columns: auto 1fr;
-    justify-content: space-between;
-    align-items: center;
-  }
-  .sub-bar {
-    display: grid;
-    grid-template-columns: auto 1fr;
-    border-bottom: 1px solid var(--black, black);
-  }
-`;
 
 export default function Header() {
   return (
