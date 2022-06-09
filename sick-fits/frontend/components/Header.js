e706fc44c41a21317236c0061719ff3ae47ef995
ryanallenk <ryanallenk@gmail.com>
2022-06-09 13:27:38 -0400

    add basic styling

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,16 +1,50 @@
+import styled from 'styled-components';
+
 import Link from 'next/link';
 import Nav from './Nav';
 
+const LogoStyles = styled.h1`
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  background: red;
+  transform: skew(-7deg);
+  a {
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 0.5rem 1rem;
+  }
+`;
+
+const HeaderStyles = styled.header`
+  .bar {
+    border-bottom: 10px solid var(--black, black);
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .sub-bar {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    border-bottom: 1px solid var(--black, black);
+  }
+`;
+
 export default function Header() {
   return (
-    <header>
-      <div className="var">
-        <Link href="/">Sick Fits</Link>
+    <HeaderStyles>
+      <div className="bar">
+        <LogoStyles>
+          <Link href="/">Sick Fits</Link>
+        </LogoStyles>
       </div>
       <div className="sub-bar">
         <p>Search</p>
       </div>
       <Nav />
-    </header>
+    </HeaderStyles>
   );
 }
