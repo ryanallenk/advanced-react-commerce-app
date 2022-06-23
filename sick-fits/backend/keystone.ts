805a26b5ee59a18858738709ecd4b321d3ed034c
ryanallenk <ryanallenk@gmail.com>
2022-06-22 20:25:13 -0400

    add seed data

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -8,6 +8,7 @@ import { User } from './schemas/User';
 import { Product } from './schemas/Product';
 import { ProductImage } from './schemas/ProductImage';
 import 'dotenv/config';
+import { insertSeedData } from './seed-data';
 
 const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/';
 
@@ -37,7 +38,12 @@ export default withAuth(
     db: {
       adapter: 'mongoose',
       url: databaseURL,
-      // TODO add data seeding here
+      async onConnect(keystone) {
+        console.log('Connected to the database.');
+        if (process.argv.includes('--seed-data')) {
+          await insertSeedData(keystone);
+        }
+      },
     },
     lists: createSchema({
       // schema items here
