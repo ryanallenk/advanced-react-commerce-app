92a136dfc844e3aced791e5eafe712f06f8864a9
ryanallenk <ryanallenk@gmail.com>
2022-07-13 16:18:24 -0400

    adjust GQL query to accept pagination values

--- sick-fits/frontend/components/Products.js
+++ sick-fits/frontend/components/Products.js
@@ -2,6 +2,7 @@ import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
 import styled from 'styled-components';
 import Product from './Product';
+import { perPage } from '../config';
 
 const ProductsListStyles = styled.div`
   display: grid;
@@ -10,8 +11,8 @@ const ProductsListStyles = styled.div`
 `;
 
 export const ALL_PRODUCTS_QUERY = gql`
-  query ALL_PRODUCTS_QUERY {
-    allProducts {
+  query ALL_PRODUCTS_QUERY($skip: Int = 0, $first: Int) {
+    allProducts(first: $first, skip: $skip) {
       id
       name
       price
@@ -26,8 +27,13 @@ export const ALL_PRODUCTS_QUERY = gql`
   }
 `;
 
-export default function Products() {
-  const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY);
+export default function Products({ page }) {
+  const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY, {
+    variables: {
+      skip: page * perPage - perPage,
+      first: perPage,
+    }
+  });
   console.log(data, error, loading);
   if (loading) return <p> Loading... </p>;
   if (error) return <p> Error: {error.message} </p>;
