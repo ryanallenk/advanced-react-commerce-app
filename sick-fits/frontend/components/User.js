65afc5b0da96781703d917262f8ade86cee1ec95
ryanallenk <ryanallenk@gmail.com>
2022-07-20 14:44:53 -0400

    add query for the current user

---
+++ sick-fits/frontend/components/User.js
@@ -0,0 +1,18 @@
+import { gql, useQuery } from '@apollo/client';
+
+export const CURRENT_USER_QUERY = gql`
+  query {
+    authenticatedItem {
+      ... on User {
+        id
+        email
+        name
+        # quert cart once we have it
+      }
+    }
+  }
+`;
+export function useUser() {
+  const { data } = useQuery(CURRENT_USER_QUERY);
+  return data?.authenticatedItem;
+}
