5621bbbb2e30610353a3ee4eae7608b3d80eaad3
ryanallenk <ryanallenk@gmail.com>
2022-08-11 12:52:45 -0400

    add signup flow and component

--- sick-fits/frontend/components/SignUp.js
+++ sick-fits/frontend/components/SignUp.js
@@ -5,53 +5,65 @@ import useForm from '../lib/useForm';
 import { CURRENT_USER_QUERY } from './User';
 import DisplayError from './ErrorMessage';
 
-const SIGNIN_MUTATION = gql`
-  mutation SIGNIN_MUTATION($email: String!, $password: String!) {
-    authenticateUserWithPassword(email: $email, password: $password) {
-      ... on UserAuthenticationWithPasswordSuccess {
-        item {
-          id
-          email
-          name
-        }
-      }
-      ... on UserAuthenticationWithPasswordFailure {
-        code
-        message
-      }
+const SIGNUP_MUTATION = gql`
+  mutation SIGNUP_MUTATION(
+    $email: String!
+    $name: String!
+    $password: String!
+  ) {
+    createUser(data: { email: $email, name: $name, password: $password }) {
+      id
+      email
+      name
     }
   }
 `;
-export default function SignIn() {
+export default function SignUp() {
   const { inputs, handleChange, resetForm } = useForm({
     email: '',
     password: '',
+    name: '',
   });
 
-  const [signin, { data, loading }] = useMutation(SIGNIN_MUTATION, {
+  const [signup, { data, loading, error }] = useMutation(SIGNUP_MUTATION, {
     variables: inputs,
     // refetch the currently logged in user
-    refetchQueries: [{ query: CURRENT_USER_QUERY }],
+    // refetchQueries: [{ query: CURRENT_USER_QUERY }],
   });
 
   async function handleSubmit(e) {
     e.preventDefault();
     console.log(inputs);
-    const res = await signin();
+    const res = await signup().catch(console.error);
     resetForm();
     console.log(res);
+    console.log({ data, loading, error });
   }
-  const error =
-    data?.authenticateUserWithPassword.__typename ===
-    'UserAuthenticationWithPasswordFailure'
-      ? data?.authenticateUserWithPassword
-      : undefined;
+
   return (
     <div>
       <Form method="POST" onSubmit={handleSubmit}>
-        <h2>Sign Into Your Account</h2>
+        <h2>Sign Up For An Account</h2>
         <DisplayError error={error} />
         <fieldset>
+          {data?.createUser && (
+            <p>
+              {' '}
+              Signed up with {data.createUser.email} - Please go ahead and sign
+              in!{' '}
+            </p>
+          )}
+          <label htmlFor="name">
+            Name
+            <input
+              type="text"
+              name="name"
+              placeholder="Your Name"
+              autoComplete="name"
+              value={inputs.name}
+              onChange={handleChange}
+            />
+          </label>
           <label htmlFor="email">
             Email
             <input
@@ -74,7 +86,7 @@ export default function SignIn() {
               onChange={handleChange}
             />
           </label>
-          <button type="submit">Sign In!</button>
+          <button type="submit">Sign Up!</button>
         </fieldset>
       </Form>
     </div>
