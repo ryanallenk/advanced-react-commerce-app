06de90939dbc234eb0446d7045d4da3aa92c287d
ryanallenk <ryanallenk@gmail.com>
2022-08-30 18:38:20 -0400

    add cart open automatically when item is added

--- sick-fits/frontend/components/AddToCart.js
+++ sick-fits/frontend/components/AddToCart.js
@@ -1,5 +1,6 @@
 import { useMutation } from '@apollo/client';
 import gql from 'graphql-tag';
+import { useCart } from '../lib/cartState';
 import { CURRENT_USER_QUERY } from './User';
 
 const ADD_TO_CART_MUTATION = gql`
@@ -15,8 +16,16 @@ export default function AddToCart({ id }) {
     variables: { id },
     refetchQueries: [{ query: CURRENT_USER_QUERY }],
   });
+  const { openCart } = useCart();
   return (
-    <button type="button" disabled={loading} onClick={addToCart}>
+    <button
+      type="button"
+      disabled={loading}
+      onClick={() => {
+        addToCart();
+        openCart();
+      }}
+    >
       Add{loading && 'ing'} to Cart 🛒
     </button>
   );
