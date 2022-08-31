551f4ffbba2591a73e48e16d5769dfdc3a958177
ryanallenk <ryanallenk@gmail.com>
2022-08-31 14:03:07 -0400

    add evicting items from cache on removal from cart

--- sick-fits/frontend/components/RemoveFromCart.js
+++ sick-fits/frontend/components/RemoveFromCart.js
@@ -1,7 +1,6 @@
 import { useMutation } from '@apollo/client';
 import gql from 'graphql-tag';
 import styled from 'styled-components';
-import { CURRENT_USER_QUERY } from './User';
 
 const BigButton = styled.button`
   font-size: 3rem;
@@ -12,6 +11,9 @@ const BigButton = styled.button`
     cursor: pointer;
   }
 `;
+function update(cache, payload) {
+  cache.evict(cache.identify(payload.data.deleteCartItem));
+}
 
 const REMOVE_FROM_CART_MUTATION = gql`
   mutation REMOVE_FROM_CART_MUTATION($id: ID!) {
@@ -24,6 +26,7 @@ const REMOVE_FROM_CART_MUTATION = gql`
 export default function RemoveFromCart({ id }) {
   const [removeFromCart, { loading }] = useMutation(REMOVE_FROM_CART_MUTATION, {
     variables: { id },
+    update,
   });
   return (
     <BigButton
